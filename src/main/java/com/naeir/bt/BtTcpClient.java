5090e56ad39a403ec227e1375272e392071bfbbc
7470 <7470@DESKTOP-FDN7TB5>
2026-05-17 15:20:44 +0300

    .

---
+++ src/main/java/com/naeir/bt/BtTcpClient.java
@@ -0,0 +1,49 @@
+package com.naeir.bt;
+
+import java.net.InetAddress;
+import java.util.Scanner;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.IoHandlerFactory;
+import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class BtTcpClient {
+    public static void main(String[] args) throws Exception {
+        InetAddress addr = InetAddress.getByName("127.0.0.1");
+        ChannelFuture future = new BtTcpClient("fa364df414550722d55d8f03071faa5fb782a3af").connect(addr, 6881);
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+
+    private String hash;
+
+    public BtTcpClient(String hash) {
+        this.hash = hash;
+    }
+
+    public ChannelFuture connect(InetAddress addr, int port) {
+        IoHandlerFactory newFactory = NioIoHandler.newFactory();
+        MultiThreadIoEventLoopGroup workerGroup = new MultiThreadIoEventLoopGroup(newFactory);
+        return new Bootstrap().group(workerGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.SO_RCVBUF, 4096)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast("he", new HandshakeEncoder());
+                        pipeline.addLast("hd", new HandshakeDecoder());
+                        pipeline.addLast("ch", new ClientHandler(BtTcpClient.this.hash));
+                    }
+                })
+                .connect(addr, port);
+    }
+}
